8aa7908d9ca4f1dcabd2fe152f3732c801262e0b
Joel Amoako <joelkofiamoako@gmail.com>
2020-03-17 07:29:24 +0000

    added workspace folder to git ignore

---
+++ Bye.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+    echo "BYYEEE..."|toilet|/usr/local/bin/lolcat
+echo "by Joel Kofi Amoako"|grep --color=always " Joel Kofi Amoako"
+sleep 1
+clear
---
+++ Greater.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "Inventory"|toilet|/usr/local/bin/lolcat
+echo "Management"|toilet|/usr/local/bin/lolcat
+echo "System"|toilet|/usr/local/bin/lolcat
+echo "by Joel Kofi Amoako"|grep --color=always " Joel Kofi Amoako"
+echo -ne '#####                     (33%)\r'|lolcat
+sleep 1
+echo -ne '#############             (66%)\r'|lolcat
+sleep 1
+echo -ne '#######################   (100%)\r'|lolcat
+echo -ne '\n'
+sleep 2
+clear
+
+
+
